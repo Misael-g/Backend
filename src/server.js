8022aaa67b4bda6b8a09bd411c281d7882d0c623
Misael-g <misael.garcia@epn.edu.ec>
2026-04-23 10:42:29 -0500

    integrar rutas de usuario y conexión a base de datos en el servidor

--- src/server.js
+++ src/server.js
@@ -4,14 +4,12 @@ import dotenv from 'dotenv'
 import cors from 'cors';
 
 
+// Importar rutas
+import routerUsuarios from './routers/usuario_routes.js'
 
 // Inicializaciones
 const app = express()
 dotenv.config()
-
-
-// Configuraciones 
-
 
 
 // Middlewares 
@@ -19,16 +17,19 @@ app.use(express.json())
 app.use(cors())
 
 
-
 // Variables globales
-app.set('port',process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000)
 
 
+// Ruta principal
+app.get('/',(req,res)=> res.send("Server on - UniBooks"))
 
-// Rutas 
-app.get('/',(req,res)=> res.send("Server on"))
+// Rutas para usuarios
+app.use('/api', routerUsuarios)
+
+// Manejo de rutas que no sean encontradas
+app.use((req, res) => res.status(404).json({ msg: 'Endpoint no encontrado - 404' }))
 
 
-
-// Exportar la instancia de express por medio de app
+// Exportar la instancia de express por medio del app
 export default  app

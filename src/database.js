eeae0c5277edf4e346fc162a014981f380698068
Misael-g <misael.garcia@epn.edu.ec>
2026-04-23 10:42:48 -0500

    mejorar configuración de conexión a MongoDB

--- src/database.js
+++ src/database.js
@@ -5,10 +5,13 @@ mongoose.set('strictQuery', true)
 const connection = async()=>{
     try {
         const {connection} = await mongoose.connect(process.env.MONGODB_URI_PRODUCTION)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
+        console.log(`Base de datos conectada en ${connection.host} - ${connection.port}`)
     } catch (error) {
-        console.log(error);
+        console.log(`Error al conectar la base de datos: ${error.message}`);
+        process.exit(1)
     }
 }
 
 export default  connection
+
+

d66b400626a3acee41d1fe6f8e002cec4fdc410b
Misael-g <misael.garcia@epn.edu.ec>
2026-06-12 18:00:27 -0500

    dejar smtp  email

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,41 +1,21 @@
 import nodemailer from 'nodemailer'
-import { google } from 'googleapis'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const OAuth2 = google.auth.OAuth2
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP
+    }
+})
 
-const createTransporter = async () => {
-    const oauth2Client = new OAuth2(
-        process.env.GOOGLE_CLIENT_ID,
-        process.env.GOOGLE_CLIENT_SECRET,
-        'https://developers.google.com/oauthplayground'
-    )
 
-    oauth2Client.setCredentials({
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-    })
-
-    const accessToken = await oauth2Client.getAccessToken()
-
-    return nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.USER_MAILTRAP,
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-            accessToken: accessToken.token
-        }
-    })
-}
 
 const sendMail = async (to, subject, html) => {
     try {
-        const transporter = await createTransporter()
         const info = await transporter.sendMail({
-            from: '"UniBooks" <unibooks08@gmail.com>',
+            from: '"UniBooks" <admin@unibooks.com>',
             to,
             subject,
             html
